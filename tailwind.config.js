b31e80f475c2fe10eee321df8cb279b09e49ec0d
allan <allansp9@gmail.com>
2021-08-29 14:11:43 -0300

    feat: add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      mobile: "360px",
+      // => @media (min-width: 360px) { ... }
+
+      desktop: "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: "#00A870",
+        secondary: "#5E20A4",
+      },
+    },
   },
   variants: {
     extend: {},

1a59769691809c352527cd8ee54294d0395cdeab
allan <allansp9@gmail.com>
2021-08-30 13:36:41 -0300

    feat: add framework section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,57 @@
 import Head from "next/head";
 import Image from "next/image";
 
+function FrameworkSection() {
+  return (
+    <div>
+      {/* mobile */}
+      <div className="block desktop:hidden py-16">
+        <div>
+          <h3 className="text-grey24 text-center px-[58px] mb-4">
+            A framework built for the long term
+          </h3>
+          <p className="body-medium text-grey24 text-center mb-6">
+            Childcare is for the long term. And you need a framework you can
+            count on that gives your share long term viability and success.
+            That’s why we’ve defined Hapu around our three tribal principles;
+            clearly defined process, transparency over money and equality of
+            participation.
+          </p>
+          <p className="text-secondary text-center px-6">
+            <a href="">Read how Hapu’s tribal background defines our app </a>
+          </p>
+        </div>
+      </div>
+      {/* desktop */}
+      <div className="hidden desktop:flex flex-col items-center py-24">
+        <div>
+          <h2 className="text-grey24 text-center px-[58px] mb-10">
+            A framework built for the long term
+          </h2>
+          <p className="body-medium text-grey24 text-center mb-10 px-[328px]">
+            Childcare is for the long term. And you need a framework you can
+            count on that gives your share long term viability and success.
+            That’s why we’ve defined Hapu around our three tribal principles;
+            clearly defined process, transparency over money and equality of
+            participation.
+          </p>
+          <p className="text-secondary text-center px-6 mb-16">
+            <a href="">Read how Hapu’s tribal background defines our app </a>
+          </p>
+        </div>
+        <div className="flex-none">
+          <Image
+            src="/images/ImageSection4.png"
+            alt="Section 1 Image"
+            height="392"
+            width="984"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function PaymentSection() {
   return (
     <div className="py-16 desktop:py-24 desktop:flex desktop:px-[228px] desktop:gap-x-11">
@@ -311,6 +362,8 @@ export default function Home() {
       <EmailSection />
       <hr className="border-2 border-grey88 h-0" />
       <PaymentSection />
+      <hr className="border-2 border-grey88 h-0" />
+      <FrameworkSection />
       <hr className="border-2 border-grey88 h-0" />
     </div>
   );

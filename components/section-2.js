17c8b0eec7e80037451ab919c2622d3aba65103a
allan <allansp9@gmail.com>
2021-08-30 16:46:00 -0300

    feat: add newsletter functionality

--- components/section-2.js
+++ components/section-2.js
@@ -1,4 +1,36 @@
+import { useState } from "react";
+import axios from "axios";
+import { set } from "lodash";
+
 function SectionTwo() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [state, setState] = useState("IDLE");
+  const [error, setError] = useState(null);
+
+  const subscribe = async (e) => {
+    e.preventDefault();
+
+    setState("LOADING");
+    setError(null);
+
+    try {
+      const response = await axios.post(
+        "https://api.jungledevs.com/api/v1/challenge-newsletter/",
+        { name, email }
+      );
+      setState("SUCCESS");
+      alert("Registered to newsletter with success!");
+
+      setName("");
+      setEmail("");
+    } catch (e) {
+      setError(e.response.data.error);
+      setState("ERROR");
+      alert("There was an error");
+    }
+  };
+
   return (
     <div className="px-4 py-16">
       <h3 className="text-center mb-4">
@@ -8,7 +40,7 @@ function SectionTwo() {
         Leave us your name and email and we’ll update you as soon as a share
         becomes available in your area!
       </p>
-      <form action="">
+      <form onSubmit={subscribe}>
         <div className="flex flex-col desktop:flex-row desktop:space-x-3 desktop:justify-center">
           <div className="h-[48px] border border-grey88 rounded-[4px] mb-2.5">
             <label htmlFor="name"></label>
@@ -17,6 +49,8 @@ function SectionTwo() {
               name="name"
               id="name"
               placeholder="Your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="pl-5 py-3.5 body-small bg-transparent placeholder-grey24"
             />
           </div>
@@ -27,11 +61,14 @@ function SectionTwo() {
               name="email"
               id="email"
               placeholder="Your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="pl-5 py-3.5 body-small bg-transparent placeholder-grey24"
             />
           </div>
           <button
             type="submit"
+            disabled={state === "LOADING"}
             className="h-[48px] desktop:w-[96px] rounded-[4px] bg-primary text-white"
           >
             Send

83d5bea6ea616d327c6b710bb48bf0a8323a558c
allan <allansp9@gmail.com>
2021-08-30 12:34:20 -0300

    feat: add payment section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,45 @@
 import Head from "next/head";
 import Image from "next/image";
 
+function PaymentSection() {
+  return (
+    <div className="py-16 desktop:py-24 desktop:flex desktop:px-[228px] desktop:gap-x-11">
+      <div className="desktop:hidden">
+        <Image
+          src="/images/ImageSection3.png"
+          alt="Section 1 Image"
+          height="230"
+          width="312"
+        />
+      </div>
+      <div className="hidden desktop:block flex-none">
+        <Image
+          src="/images/ImageSection3.png"
+          alt="Section 1 Image"
+          height="336"
+          width="456"
+        />
+      </div>
+
+      <div>
+        <h3 className="text-center px-[67px] mt-10 mb-4 text-grey24">
+          Shared payments made simple
+        </h3>
+        <p className="body-medium text-grey24 text-center mb-6">
+          Sometimes it’s hard enough just sharing a restaurant bill. Try sharing
+          that bill week in, week out and you might encounter more than a few
+          snares. But not with Hapu. Simply set your rates and our automated
+          payment system takes care of the rest. You need never talk money or
+          who owes what.
+        </p>
+        <p className="text-secondary text-center">
+          <a href="">Read how Bridget’s share (without Hapu) ended over $15</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
 function EmailSection() {
   return (
     <div className="px-4 py-16">
@@ -270,6 +309,8 @@ export default function Home() {
       <Section1 />
       <hr className="border-2 border-grey88 h-0" />
       <EmailSection />
+      <hr className="border-2 border-grey88 h-0" />
+      <PaymentSection />
       <hr className="border-2 border-grey88 h-0" />
     </div>
   );

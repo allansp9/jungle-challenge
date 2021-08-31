ad8b5e14f274219ddb53931db7e1c12edda334e5
allan <allansp9@gmail.com>
2021-08-31 17:12:16 -0300

    feat: add lang attr to html tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,12 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
           />
         </Head>
